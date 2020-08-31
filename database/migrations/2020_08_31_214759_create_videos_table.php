d7940fbe67c65cd9c4fcf4e6816269e8050e5a83
XIAC <x.i.a.c.1504@gmail.com>
2020-08-31 18:42:59 -0400

    Migraciones

---
+++ database/migrations/2020_08_31_214759_create_videos_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('titulo');
+            $table->string('descripcion');
+            $table->string('ruta_video', 100);
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return voids
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
---
+++ database/migrations/2020_08_31_220737_create_comentarios_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateComentariosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comentarios', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('contenido');
+            $table->integer('user_id')->unsigned();
+            $table->integer('video_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('video_id')->references('id')->on('videos');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comentarios');
+    }
+}
